ae0c5836af2b7b7fa85791a1fcf041b6b6140a4f
Willy Ricardo Wijaya <willyric17@gmail.com>
2024-08-01 17:57:04 +0700

    create sample app for theme preview

---
+++ example/theme/src/main/java/id/kokowilly/filebrowser/theme/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.kokowilly.filebrowser.theme
+
+import android.os.Bundle
+import id.kokowilly.filebrowser.style.ImmersiveActivity
+
+class MainActivity : ImmersiveActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
