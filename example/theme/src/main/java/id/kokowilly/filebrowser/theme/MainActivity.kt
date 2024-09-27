cc1e9224338f85125ada5a252c69c47b0128c5be
Willy Ricardo Wijaya <willyric17@gmail.com>
2024-09-27 09:10:39 +0700

    feature: upgrade to Material 3 theme

--- example/theme/src/main/java/id/kokowilly/filebrowser/theme/MainActivity.kt
+++ example/theme/src/main/java/id/kokowilly/filebrowser/theme/MainActivity.kt
@@ -1,7 +1,7 @@
 package id.kokowilly.filebrowser.theme
 
 import android.os.Bundle
-import id.kokowilly.filebrowser.foundation.ImmersiveActivity
+import id.kokowilly.filebrowser.foundation.style.ImmersiveActivity
 
 class MainActivity : ImmersiveActivity() {
 
